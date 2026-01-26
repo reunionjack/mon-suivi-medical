d5bd5f6a9ff17d4a90325047e2a0d086d88d5727
reunionjack <123633713+reunionjack@users.noreply.github.com>
2026-01-26 09:36:09 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = 'med-v3';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'med-v5'; // On change la version pour forcer la mise à jour
+const ASSETS = [
+  'index.html', 
+  'manifest.json', 
+  'Logo3.png', 
+  'icon-192.png', 
+  'icon-512.png'
+];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'SCHEDULE_NOTIF') {
-        // Le délai est géré ici pour le rappel du lendemain
-        setTimeout(() => {
-            self.registration.showNotification(event.data.title, {
-                body: event.data.body,
-                icon: 'https://cdn-icons-png.flaticon.com/512/822/822143.png',
-                vibrate: [200, 100, 200]
-            });
-        }, event.data.delay);
-    }
-});
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'SCHEDULE_NOTIF') {
+        self.registration.showNotification(event.data.title, {
+            body: event.data.body,
+            icon: 'icon-192.png', // Utilise votre nouvelle icône locale
+            vibrate: [200, 100, 200]
+        });
+    }
 });
